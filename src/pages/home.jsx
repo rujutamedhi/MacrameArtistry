31ea6ab4099e8906c380ad358cd2b70dbfb8c422
rujutamedhi <rujutamedhi@gmail.com>
2024-09-18 19:50:34 +0530

    added json

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,88 +1,37 @@
-import React from 'react'
-import { Card,Button } from 'react-bootstrap';
-// import { Card, CardContent, Typography } from '@mui/material';
-import butterfly1 from"../images/butterfly1.jfif"
-import butterfly2 from"../images/butterfly2.jfif"
-import butterfly3 from"../images/butterfly3.jfif"
-import butterfly4 from"../images/butterfly4.jfif"
-import './home.css'
-export default function home() {
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+import data from '../category.json';  // Import JSON data
+import './home.css';
+
+// Function to import all images from the 'images' folder
+const importAll = (r) => {
+  let images = {};
+  r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+  return images;
+};
+
+// Update the regex to match image file types you want to support
+const images = importAll(require.context('../images', false, /\.(png|jpe?g|jfif)$/));
+
+export default function Home() {
   return (
     <div className='cards'>
-        
-        <Card className='card' style={{  }}>
-      <Card.Img className='cardimg' variant="top" src={butterfly1}/>
-      <Card.Body>
-        <Card.Title>BUTTERFLY</Card.Title>
-        <Card.Text>
-          <Button className='button'>View more</Button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card className='card' style={{  }}>
-      <Card.Img className='cardimg' variant="top" src={butterfly2}/>
-      <Card.Body>
-        <Card.Title>BUTTERFLY</Card.Title>
-        <Card.Text>
-          <Button className='button'>View more</Button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card className='card' style={{  }}>
-      <Card.Img className='cardimg' variant="top" src={butterfly3}/>
-      <Card.Body>
-        <Card.Title>BUTTERFLY</Card.Title>
-        <Card.Text>
-          <Button className='button' >View more</Button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card className='card' style={{  }}>
-      <Card.Img className='cardimg' variant="top" src={butterfly4}/>
-      <Card.Body>
-        <Card.Title>BUTTERFLY</Card.Title>
-        <Card.Text>
-          <Button className='button'>View more</Button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    
-    <Card className='card' style={{  }}>
-      <Card.Img className='cardimg' variant="top" src={butterfly4}/>
-      <Card.Body>
-        <Card.Title>BUTTERFLY</Card.Title>
-        <Card.Text>
-          <Button className='button'>View more</Button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card className='card' style={{  }}>
-      <Card.Img className='cardimg' variant="top" src={butterfly4}/>
-      <Card.Body>
-        <Card.Title>BUTTERFLY</Card.Title>
-        <Card.Text>
-          <Button className='button'>View more</Button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card className='card' style={{  }}>
-      <Card.Img className='cardimg' variant="top" src={butterfly4}/>
-      <Card.Body>
-        <Card.Title>BUTTERFLY</Card.Title>
-        <Card.Text>
-          <Button className='button'>View more</Button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card className='card' style={{  }}>
-      <Card.Img className='cardimg' variant="top" src={butterfly4}/>
-      <Card.Body>
-        <Card.Title>BUTTERFLY</Card.Title>
-        <Card.Text>
-          <Button className='button'>View more</Button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+      {data.map((item, index) => (
+        <Card key={index} className='card'>
+          <Card.Img 
+            className='cardimg' 
+            variant="top" 
+            src={images[item.img]} 
+            alt={item.name}
+          />
+          <Card.Body>
+            <Card.Title>{item.name}</Card.Title>
+            <Card.Text>
+              <Button className='button'>View more</Button>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
-  )
+  );
 }
